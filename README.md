b8d5262761aae8a51e4b46efe8cab88b9ee02197
Marcus Näslund <naslundx@gmail.com>
2018-04-24 09:01:07 +0200

    Specialized pylint class

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Use a third-party tool to review authors of a repo
 - Find better solution for pylint.rc
 - Pass targets properly
 - Only scan changed files for performance
+- Write a better readme
+- Write generic version and pylint specialization
+- Check pylint installed
+- Add C++ support
--- gitreview.py
+++ gitreview.py
@@ -9,51 +9,64 @@ import os
 from collections import Counter
 
 
-def run_pylint(target, cwd):
-    pylint_proc = subprocess.run("pylint -j4 {} --rcfile=pylint.rc".format(target), cwd=cwd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    output = [line for line in pylint_proc.stdout.split('\n') if line.strip()]
-    return output
+# run(): Run the analysis, return the output
+# get_score(output): Extract the score as float
+# match_errors(before, after): Find difference in errors, return map name:occurences
+
+class Analysis_Pylint:
+    def __init__(self, cwd, target):
+        self.cwd = cwd
+        self.target = target
+        shutil.copyfile('pylint.rc', os.path.join(cwd, 'pylint.rc'))
+    
+    def run(self):
+        pylint_proc = subprocess.run("pylint -j4 {} --rcfile=pylint.rc".format(self.target), cwd=self.cwd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        output = [line for line in pylint_proc.stdout.split('\n') if line.strip()]
+        return output
+
+    def get_score(self, output):
+        match = re.match(r'.*(\d\.\d\d)/10.*', output[-1])
+        score = -100.0
+        if match:
+            score = float(match.groups()[0])
+        return score
+
+    def match_errors(self, before, after):
+        before_errors = []
+        after_errors = []
+        for line in before:
+            py_match = re.match(r'.*\((.+)\)$', line.strip())
+            if py_match and "previous run" not in line:
+                before_errors.append(py_match.groups()[0])
+        for line in after:
+            py_match = re.match(r'.*\((.+)\)$', line.strip())
+            if py_match and "previous run" not in line:
+                after_errors.append(py_match.groups()[0])
+
+        before_errors = Counter(before_errors)
+        after_errors = Counter(after_errors)
+
+        diff_errors = {}
+        for name in after_errors:
+            if name not in before_errors:
+                diff = after_errors[name]
+            else:
+                diff = after_errors[name] - before_errors[name]
+
+            if diff != 0:
+                diff_errors[name] = diff
+
+        return diff_errors
 
 
-def get_pylint_score(output):
-    match = re.match(r'.*(\d\.\d\d)/10.*', output[-1])
-    score = -100.0
-    if match:
-        score = float(match.groups()[0])
-    return score
-
-
-def match_pylint_errors(before, after):
-    before_errors = []
-    after_errors = []
-    for line in before:
-        py_match = re.match(r'.*\((.+)\)$', line.strip())
-        if py_match and "previous run" not in line:
-            before_errors.append(py_match.groups()[0])
-    for line in after:
-        py_match = re.match(r'.*\((.+)\)$', line.strip())
-        if py_match and "previous run" not in line:
-            after_errors.append(py_match.groups()[0])
-
-    before_errors = Counter(before_errors)
-    after_errors = Counter(after_errors)
-
-    diff_errors = {}
-    for name in after_errors:
-        if name not in before_errors:
-            diff = after_errors[name]
-        else:
-            diff = after_errors[name] - before_errors[name]
-
-        if diff != 0:
-            diff_errors[name] = diff
-
-    return diff_errors
+def run_process(command, cwd):
+    proc = subprocess.run(command, cwd=cwd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    return str(proc.stdout)
 
 
 def check_stats(cwd):
-    stats_proc = subprocess.run("git diff HEAD HEAD~1 --stat", cwd=cwd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    output = [line.strip() for line in stats_proc.stdout.split('\n') if line.strip()]
+    output = run_process("git diff HEAD HEAD~1 --stat", cwd)
+    output = [line.strip() for line in output.split('\n') if line.strip()]
 
     total = 0
     for line in output:
@@ -97,33 +110,36 @@ def output_stats(stats):
 def review(options):
     stats = {}
     diff_errors = {}
-    after_output = run_pylint(options.target, options.cwd)
-    before_output = after_output
-    after_score = get_pylint_score(after_output)
-    before_score = after_score
+
+    if options.tool == 'pylint':
+        tool = Analysis_Pylint(options.cwd, options.target)
+    else:
+        print('Unrecognized tool: {}'.format(options.tool))
+        return 1
+
+    after_output = before_output = tool.run()
+    after_score = before_score = tool.get_score(after_output)
 
     for i in range(options.iterations):
         print("\n=== Iteration {} ===".format(i))
 
         # get author
-        author_proc = subprocess.run("git log -1 --pretty=format:'%an'", cwd=options.cwd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-        author = str(author_proc.stdout)
+        author = run_process("git log -1 --pretty=format:'%an'", options.cwd)
 
         # get commit hash
-        hash_proc = subprocess.run("git rev-parse HEAD", cwd=options.cwd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-        commit = str(hash_proc.stdout)
+        commit = run_process("git rev-parse HEAD", options.cwd)
 
         # get stats
         lines_changed = check_stats(options.cwd)
 
         # revert latest commit
-        subprocess.run("git reset --hard HEAD~1", cwd=options.cwd, shell=True, check=True, stdout=subprocess.PIPE)
+        run_process("git reset --hard HEAD~1", options.cwd)
 
         # update the score if python scripts changed
         if lines_changed > 0:
-            before_output = run_pylint(options.target, options.cwd)
-            before_score = get_pylint_score(before_output)
-            diff_errors = match_pylint_errors(before_output, after_output)
+            before_output = tool.run()
+            before_score = tool.get_score(before_output)
+            diff_errors = tool.match_errors(before_output, after_output)
 
         # compute the diff
         diff = after_score - before_score
@@ -163,6 +179,12 @@ def main():
         action='store',
         help="Iterations to perform")
     format_group.add_argument(
+        "--tool",
+        default='pylint',
+        action='store',
+        help='Which tool to use'
+    )
+    format_group.add_argument(
         "target",
         default=["*.py", "**/*.py"],
         nargs="*")
@@ -170,9 +192,8 @@ def main():
     options = arg_parser.parse_args(sys.argv[1:])
     options.target = ' '.join(options.target)
     options.iterations = int(options.iterations)
-    shutil.copyfile('pylint.rc', os.path.join(options.cwd, 'pylint.rc'))
-    review(options)
+    return review(options)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
